4f628f07f24797725514f7199c045ff609658b50
abhik2003 <abhik03mj@gmail.com>
2022-03-26 11:38:50 +0530

    Added Temp Converter Project

---
+++ javascript/4.js
@@ -0,0 +1,10 @@
+function fun()
+        {
+        let number1;number1=Number(document.getElementById('n1').value);
+        let number2;number2=Number(document.getElementById('n2').value);
+        let res;res=number1+number2;
+        // let number1;number1=document.getElementById('n1').value;
+        // let number2;number2=document.getElementById('n2').value;
+        // let res;res=number1*1+number2*1;
+        alert('Sum of these numbers is '+res);
+        }
---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+//prompt
+
+// var a=prompt('Enter your name:');
+// if(a==null||a=='')
+// a='visitor'
+// document.querySelectorAll('.div')[0].innerHTML="Hi "+ a;
+
+
+//time alert
+
+// var time=5;
+// display_time=()=>{
+//    alert('You have spent '+time+' seconds');
+//    time=time+5;
+// }
+// setInterval(display_time,5000);
+
+
